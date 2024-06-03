29ca50bb648f8c5c6e43e0518e697154378ec8f5
Stephen Goncalves <stiefo.o@gmail.com>
2024-06-03 21:55:30 +0100

    feat: adds config package with custom logger and init db func

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"github.com/stgonzles/drillit-api/config"
 	"github.com/stgonzles/drillit-api/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
-	
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Failed to load db config: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
